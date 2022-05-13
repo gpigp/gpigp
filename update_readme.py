e457424c58412407bd7405c0130281da15ed991d
김태현 <38418878+gpigp@users.noreply.github.com>
2022-05-13 17:40:25 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -9,13 +9,18 @@ for idx, feed in enumerate(url_feed['entries']):
     if idx > MAX_POST_NUM:
         break
     feed_date = feed['published_parsed']
-    latest_list += f"###[    ▪{feed_date.tm_year}.{feed_date.tm_mon}.{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+    if feed_date.tm_mon < "10":
+        feed_date.tm_mon += "0"
+    if feed_date.tm_mday < "10":
+        feed_date.tm_mday += "0"
+        
+    latest_list += f"### [    ▪{feed_date.tm_year}.{feed_date.tm_mon}.{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
 
 markdown_1 = """[![header](https://capsule-render.vercel.app/api?type=waving&color=auto&height=150&section=header&text=KIM%20TAEHYUN%20🌱&fontSize=40&fontColor=392f31)](https://gpigp.github.io/taehyun)
 
 [![TAEHYUN's GitHub stats](https://github-readme-stats.vercel.app/api?username=gpigp&show_icons=true&theme=vue)](https://github.com/gpigp)
 
-📋Recent Blog Post<br>
+## 📋Recent Blog Post<br>
 """
 
 markdown_2 = """## 🛠 Skills 🛠  
