977025ebf712bfd9141530317f90d62a61b028e2
김태현 <38418878+gpigp@users.noreply.github.com>
2022-05-14 00:24:05 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -22,7 +22,7 @@ for idx, feed in enumerate(url_feed['entries']):
     else:
         day_ = str(feed_date.tm_mday)
         
-    latest_list += '<a href="' + feed['link'] + '">' + feed_date.tm_year + '.' + mon_ + '.' + day_ + '\t' + feed['title'] + '</a><br>\n'
+    latest_list += '<a href="' + feed['link'] + '">' + str(feed_date.tm_year) + '.' + mon_ + '.' + day_ + '\t' + feed['title'] + '</a><br>\n'
 
 markdown_1 = """[![header](https://capsule-render.vercel.app/api?type=waving&color=auto&height=150&section=header&text=KIM%20TAEHYUN%20🌱&fontSize=40&fontColor=392f31)](https://gpigp.github.io/taehyun)
 
